80565de5c9eb499140e15ca8da8fab45b3bec822
Maksim Rastoskuev <maksimrastoskuev@ya.ru>
2025-01-10 21:25:13 +0700

    add compareData and file modification

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import gendiff from '../src/index.js';
+import genDiff from '../src/index.js';
 const program = new Command();
 
 program
@@ -11,7 +11,7 @@ program
   .argument('<filepath2>')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+    console.log(genDiff(filepath1, filepath2));
   });
   
 program.parse();
---
+++ src/compareData.js
@@ -0,0 +1,29 @@
+import _ from 'lodash'
+
+const compareData = (data1, data2) => {
+  const data1keys = _.keys(data1);
+  const data2keys = _.keys(data2);
+  const keys = _.union(data1keys, data2keys);
+  const sortedKeys = _.sortBy(keys);
+
+  const distinctions = sortedKeys.reduce((acc, key) => {
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] === data2[key]) {
+        acc[`  ${key}`] = data1[key];
+      } else {
+        acc[`- ${key}`] = data1[key];
+        acc[`+ ${key}`] = data2[key];
+      }
+    } else if (_.has(data1, key) && !_.has(data2, key)) {
+      acc[`- ${key}`] = data1[key];
+    } else {
+      acc[`+ ${key}`] = data2[key];
+    }
+
+    return acc;
+  }, {});
+
+  return distinctions;
+};
+
+export default compareData;
