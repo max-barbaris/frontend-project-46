4974cc23d815c642a6e588a2fd08f7c1f351c89f
Maksim Rastoskuev <maksimrastoskuev@ya.ru>
2025-01-18 17:09:27 +0700

    added tests on yaml and yaml format files

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -9,9 +9,17 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff', () => {
+test('json file format', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
+  const expected = readFile('result.txt');
+
+  expect(genDiff(file1, file2)).toEqual(expected);
+});
+
+test('yaml file format', () => {
+  const file1 = getFixturePath('file1.yml');
+  const file2 = getFixturePath('file2.yml');
   const expected = readFile('result.txt');
 
   expect(genDiff(file1, file2)).toEqual(expected);
