47704833aac228e57fe418ad74f794b8d51332d3
Maksim Rastoskuev <maksimrastoskuev@ya.ru>
2025-01-18 17:16:18 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import parse from './parse.js';
+import fileParsing from './parsers.js';
 import compareData from './compareData.js';
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+const fileFormat = (filepath) => path.extname(filepath);
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = readFile(filepath1);
   const data2 = readFile(filepath2);
-  const parsedData1 = parse(data1);
-  const parsedData2 = parse(data2);
-  const result = compareData(parsedData1, parsedData2);
+  const parsedData1 = fileParsing(data1, fileFormat(filepath1));
+  const parsedData2 = fileParsing(data2, fileFormat(filepath2));
+  const comparsion = compareData(parsedData1, parsedData2);
 
-  return result;
+  return comparsion;
 };
 
 export default genDiff;
